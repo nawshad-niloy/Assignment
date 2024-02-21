ee0c5ae0f60db07b8f14b161d7765c6a99735c6b
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2024-02-22 01:13:33 +0600

    Add files via upload

---
+++ 2231081055_problem_4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace problem 
+{
+
+    class Hello 
+    {
+
+        static void Main(string[] args)
+        {
+            int[] numbers = {45, 89, 80, 15, 23, 24, 66, 49, 38, 77};
+            int sum = 0;
+            int min = numbers[0];
+            for(int i = 0; i < numbers.Length;i++)
+            {
+                sum += numbers[i];
+                if(numbers[i]<min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            double avgr = sum/(double)numbers.Length;
+
+            Console.WriteLine($"The Average of the numbers of Array : {avgr}");
+            Console.WriteLine($"The minimum number from the array : {min}");
+        }
+    }
+}
+
