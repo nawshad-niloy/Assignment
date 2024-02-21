4dc9ac5f6ed905bc69e5fd5f21ba2975745d7b69
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2024-02-22 00:50:29 +0600

    Add files via upload

---
+++ 2231081055_problem3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Transactions;
+namespace problem 
+{
+
+    class Hello 
+    {
+
+        static void Main(string[] args)
+        {
+           Console.Write("Enter the first number of the series : ");
+           int n = Convert.ToInt32(Console.ReadLine());
+           for(int i = n;i >= 0; i--)
+           {
+                Console.Write(i + " ");
+           }
+        }
+    }
+}
+
