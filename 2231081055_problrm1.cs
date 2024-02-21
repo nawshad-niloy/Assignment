62e208deb552a27c157e99f491abed1e8a5c8d0e
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2024-02-22 00:04:12 +0600

    Add files via upload

---
+++ 2231081055_problrm1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Enter the first number : ");
+           int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a+b;
+            Console.WriteLine("{0} + {1} = {2}",a,b,sum);
+
+            int sub = a-b;
+            Console.WriteLine("{0} - {1} = {2}",a,b,sub);
+
+            int multi = a*b;
+            Console.WriteLine("{0} x {1} = {2}",a,b,multi);
+
+            double div = a/b;
+            Console.WriteLine("{0} x {1} = {2:#.##}",a,b,div);
+
+        }
+    }
+}
