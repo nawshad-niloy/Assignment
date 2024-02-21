78d7edd1e5c19676330ba8ebaceb477bb81bde89
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2024-02-22 00:26:49 +0600

    Add files via upload

---
+++ 2231081055_problem2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace problem 
+{
+
+    class Hello 
+    {
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for(int i = 5;i<=500;i=i+5)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
